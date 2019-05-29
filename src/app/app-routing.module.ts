49c4ce739bd401d23c2336600ddfbf0de1513950
Adrian Gomez <agomezdo18dw@ikzubirimanteo.com>
2019-05-29 10:29:13 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'idiomas', loadChildren: './idiomas/idiomas.module#IdiomasPageModule' },
+  { path: 'titulos', loadChildren: './titulos/titulos.module#TitulosPageModule' },
+  { path: 'experiencia', loadChildren: './experiencia/experiencia.module#ExperienciaPageModule' }
 ];
 @NgModule({
   imports: [
